a1a555585632647fc9a56dc42c27c1529ba7bcad
StanDoge <stanlydoge@gmail.com>
2023-05-05 23:07:40 -0600

    project boilerplate

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ama_zon.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
