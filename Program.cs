e1f10af87aa691257b079a95e2513f0086909b69
StanDoge <stanlydoge@gmail.com>
2023-05-19 14:02:12 -0600

    `feat:` dependecy injenction for db connection
    closes #21

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using ama_zon;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AmaZonContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["hdp-server:ConnectionString"]);
+});
 
 var app = builder.Build();
 
