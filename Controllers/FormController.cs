b9e82fe6712d878793114b67ed814695087d9bf4
StanDoge <stanlydoge@gmail.com>
2023-05-23 09:39:47 -0600

    `chore:` append library for doc manipulation

---
+++ Controllers/FormController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spire.Doc;
+
+namespace ama_zon.Controllers;
+
+public class FormController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
