288dd1c596dc818c3ded29beb41a04f4fc102c15
StanDoge <stanlydoge@gmail.com>
2023-05-28 11:07:19 -0600

    class with logic for mail feature

---
+++ Controllers/EmailSender.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Mail;
+namespace ama_zon.Controllers;
+
+public class EmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string receiver, string subject, string message)
+    {
+        string sender = "";
+        string password = "";
+
+        var client = new SmtpClient("smtp.gmail.com", 587)
+        {
+            EnableSsl = true,
+            Credentials = new NetworkCredential(sender, password)
+        };
+
+        return client.SendMailAsync(
+            new MailMessage(
+                from: sender,
+                to: receiver,
+                subject,
+                message));
+    }
+}
