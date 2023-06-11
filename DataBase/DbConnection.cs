5a8ee6fd47ddca92c1d7dbc8b423f3f8e31616e8
KeniaEstevez2023 <ee08004@ues.edu.sv>
2023-06-10 22:32:02 -0600

    Funcionalidad uploadTextFile

---
+++ DataBase/DbConnection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ama_zon.DataBase
+{
+    public class DbConnection
+    {
+        private readonly string _connectionString;
+
+        public DbConnection(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public SqlConnection OpenConnection()
+        {
+            var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        public void CloseConnection(SqlConnection connection)
+        {
+            if (connection != null && connection.State != ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
