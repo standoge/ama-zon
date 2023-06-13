dd0cb451b30bbf1f142cf9bd139a83b74325c759
pedroUmanzor <ug20004@ues.edu.sv>
2023-06-12 19:56:15 -0600

    Confirm

--- Controllers/PageUI2Controller.cs
+++ Controllers/PageUI2Controller.cs
@@ -13,22 +13,23 @@ namespace ama_zon.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(string Nombre, string Apellidos, string Direccion, DateTime Fecha)
+        public ActionResult Index(string Nombre, string Apellidos, string Direccion, DateTime Fecha, string pais)
         {
             string nombre = Nombre + " " + Apellidos;
             Document doc = new Document("Controllers/Templates/permanent-template.docx");
             Document newdoc = doc.Clone();
-            newdoc.SaveToFile("Controllers/Templates/output.docx");
+            newdoc.SaveToFile("Controllers/Templates/"+nombre+".docx");
             Document contrato = new Document();
-            contrato.LoadFromFile("Controllers/Templates/output.docx");
+            contrato.LoadFromFile("Controllers/Templates/"+nombre+".docx");
             contrato.Replace("$nombre_receptor", nombre, false, true);
             contrato.Replace("$direccion_receptor", Direccion, false, true);
             contrato.Replace("$fecha_emision", Fecha.ToString("dd MMMM, yyyy"), false, true);
-            contrato.SaveToFile("Controllers/Templates/output.docx");
+            contrato.Replace("$pais", pais, false, true);
+            contrato.SaveToFile("Controllers/Templates/" + nombre + ".docx");
             contrato.Close();
 
-            contrato.LoadFromFile("Controllers/Templates/output.docx");
-            contrato.SaveToFile("Controllers/Templates/output.pdf", FileFormat.PDF);
+            contrato.LoadFromFile("Controllers/Templates/" + nombre + ".docx");
+            contrato.SaveToFile("Controllers/Templates/"+nombre+".pdf", FileFormat.PDF);
             return View("Index");
         }
 
